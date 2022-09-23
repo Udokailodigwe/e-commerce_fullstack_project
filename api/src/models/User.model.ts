d5258a13e0364c9905b8cd7c3594d224429d2922
Udokailodigwe <ilodigweudoka@gmail.com>
2022-09-23 14:43:32 +0300

    Created user model

---
+++ api/src/models/User.model.ts
@@ -0,0 +1,51 @@
+import mongoose, { Document } from 'mongoose'
+
+export type UserDocument = Document & {
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+  image: string
+  product: mongoose.Schema.Types.ObjectId[]
+  isBanned: boolean
+}
+
+const userSchema = new mongoose.Schema({
+  product: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'Product',
+  },
+
+  firstName: {
+    type: String,
+    required: true,
+  },
+
+  lastName: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: [true, 'Email required'],
+    trim: true,
+    lowercase: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  image: {
+    type: String,
+  },
+
+  isBlocked: {
+    default: false,
+  },
+})
+
+export default mongoose.model<UserDocument>('User', userSchema)
