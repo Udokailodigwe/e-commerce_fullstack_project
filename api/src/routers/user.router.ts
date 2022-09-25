caedffc53cf5a0ffda0052e7c8dba713c8d8403b
Udokailodigwe <ilodigweudoka@gmail.com>
2022-09-25 21:24:15 +0300

    Router for all users endpoints complete.

--- api/src/routers/user.router.ts
+++ api/src/routers/user.router.ts
@@ -1,11 +1,20 @@
 import express from 'express'
 
-import { findAll, createUser } from '../controllers/User.controller'
+import {
+  findAll,
+  createUser,
+  updateUser,
+  findById,
+  deleteUser,
+} from '../controllers/User.controller'
 
 const router = express.Router()
 
 // Every path we define here will get /api/v1/users prefix
 router.get('/', findAll)
 router.post('/', createUser)
+router.put('/:userId', updateUser)
+router.get('/:userId', findById)
+router.delete('/:userId', deleteUser)
 
 export default router
