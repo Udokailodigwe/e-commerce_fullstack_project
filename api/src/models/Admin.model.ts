d7580a4ff562737b13bc0972db6ad520973dd48f
Udokailodigwe <ilodigweudoka@gmail.com>
2022-09-26 00:05:36 +0300

    Admin model created

---
+++ api/src/models/Admin.model.ts
@@ -0,0 +1,38 @@
+import mongoose, { Document } from 'mongoose'
+
+export type AdminDocument = Document & {
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+}
+
+const AdminSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    minlength: 2,
+    required: true,
+  },
+
+  lastName: {
+    type: String,
+    minlength: 2,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: [true, 'Email required'],
+    trim: true,
+    lowercase: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    minlength: 2,
+    required: true,
+  },
+})
+
+export default mongoose.model<AdminDocument>('Adin', AdminSchema)
