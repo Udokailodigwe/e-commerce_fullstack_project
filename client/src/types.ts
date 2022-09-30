3bf7e69477e953fc1e038cda4abdf64b41276e60
Udokailodigwe <ilodigweudoka@gmail.com>
2022-09-30 13:27:56 +0300

    Adjusted key category and size values to proper types

--- client/src/types.ts
+++ client/src/types.ts
@@ -2,14 +2,38 @@ export type Product = {
   _id?: string;
   name: string;
   description: string;
-  category: string[];
+  category: {
+    kids: string[];
+    women: string[];
+    men: string[];
+  };
   variants: string[];
-  sizes: string[];
-  images: string;
+  sizes: {
+    small: number;
+    medium: number;
+    large: number;
+    xlarge: number;
+  };
+  image: string;
 };
 
 export type ProductState = {
-  productData: Product[];
+  products: Product[];
   isLoading: boolean;
   error: boolean;
+};
+
+export type NewProduct = {
+  _id?: string;
+  name: string;
+  description: string;
+  kids: string[];
+  women: string[];
+  men: string[];
+  variants: string[];
+  small: number;
+  medium: number;
+  large: number;
+  xlarge: number;
+  image: string;
 };
