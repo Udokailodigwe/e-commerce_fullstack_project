9ff2207fc576e9b33cf33660a347674954584314
Udokailodigwe <ilodigweudoka@gmail.com>
2022-10-07 14:23:15 +0300

    Added more routes

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import AdminProducts from "components/AdminProduct/AdminProducts";
+import AdminUsers from "components/AdminUser/AdminUsers";
+import User from "components/AdminUser/user";
 
 const AppRoutes = () => {
   return (
@@ -11,6 +13,8 @@ const AppRoutes = () => {
         <Route path="/products/" element={<Home />} />
         <Route path="/products/:productId" element={<Product />} />
         <Route path="/adminproducts/" element={<AdminProducts />} />
+        <Route path="/adminusers/" element={<AdminUsers />} />
+        <Route path="/users/:userId" element={<User />} />
       </Routes>
     </div>
   );
